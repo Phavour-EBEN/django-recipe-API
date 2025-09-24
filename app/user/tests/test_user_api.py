265bcd421aa0015dcf17deec65bcf57e220f5f6a
Phavour-EBEN <ainooebenezer05@gmail.com>
2025-09-24 02:24:25 +0000

    implemented the user api endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -38,8 +38,8 @@ class PublicUserApiTests(TestCase):
             'password':'testpass123',
             'name':'Test Name'
         }
+        create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
-
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
     
     def test_password_too_short_error(self):
