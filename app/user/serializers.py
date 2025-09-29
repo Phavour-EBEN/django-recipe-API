49e2c7c2b22acde0af4dab38dbd04d6cdfa194fb
Phavour-EBEN <ainooebenezer05@gmail.com>
2025-09-29 23:54:04 +0000

    implemented the test for manage users

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -15,6 +15,17 @@ class UserSerializer(serializers.ModelSerializer):
         """create and return a user with an encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return the user"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """serializer for the user auth token"""
