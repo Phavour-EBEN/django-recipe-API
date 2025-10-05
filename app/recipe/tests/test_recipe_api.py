bee94fab0d2e135f702d18f8db757e623f7f14e7
Phavour-EBEN <ainooebenezer05@gmail.com>
2025-10-05 23:15:04 +0000

    Implemented the recipe model

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,14 @@
+"""
+Test for recipe APIs
+"""
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Recipe
+from recipe.serializers import RecipeSerializer
