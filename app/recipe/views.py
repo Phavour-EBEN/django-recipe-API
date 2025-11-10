7cddfc3f88339a22cda745fa3ff469453f0b57ca
Phavour-EBEN <ainooebenezer05@gmail.com>
2025-11-10 00:09:02 +0000

    Implemented the update recipe tag

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -36,7 +36,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         )
         
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.UpdateModelMixin ,mixins.ListModelMixin, viewsets.GenericViewSet):
     """Manage Tags in the database"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
